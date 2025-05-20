ebcbf49a412623ce4b2c1ddfd84470a41a65f2b3
dmitriydrozhzhin <drozhzhindmitriy@icloud.com>
2025-05-20 09:24:01 +0300

    Day 1, Task 4

--- src/Day1/Task4.java
+++ src/Day1/Task4.java
@@ -11,8 +11,9 @@ package Day1;
 public class Task4 {
     public static void main(String[] args) {
         int year = 1980;
-        for (int i = year; i<=2020;i+=4){
-            System.out.println("Олимпиада "+i+" года");
+        while (year<=2020){
+            System.out.println("Олимпиада "+year+" года");
+            year+=4;
         }
     }
 }
