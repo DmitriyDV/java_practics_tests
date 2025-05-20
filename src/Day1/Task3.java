e316eb2d02abc6efc01bca18e0490d662f64e912
dmitriydrozhzhin <drozhzhindmitriy@icloud.com>
2025-05-20 09:08:55 +0300

    Day 1, Task 3

---
+++ src/Day1/Task3.java
@@ -0,0 +1,24 @@
+/**
+ * Вывест и на экран слово "JAVA", в столбик, чтобы оно повторилось 10 раз, используя цикл на ваше усмотрение
+ * (for or while)
+ * Вывод текста должен быть таким:
+ * JAVA
+ * JAVA
+ * JAVA
+ * JAVA
+ * JAVA
+ * JAVA
+ * JAVA
+ * JAVA
+ * JAVA
+ * JAVA
+ */
+package Day1;
+
+public class Task3 {
+    public static void main(String[] args) {
+        for (int i=0;i<10;i++){
+            System.out.println("JAVA");
+        }
+    }
+}
